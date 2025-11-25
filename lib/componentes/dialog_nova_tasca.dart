adcdd2072623f8025687b9041d764a59bfbda19d
Kevin Yamel Díaz Pérez <kevindiaz@ceroca.cat>
2025-11-25 15:58:20 +0100

    changes

---
+++ lib/componentes/dialog_nova_tasca.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DialogNovaTasca extends StatelessWidget {
+  const DialogNovaTasca({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog();
+  }
+}
